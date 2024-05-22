dbda546306becdda74db59a0ffc98bb65813a630
Philippe Serhal <philippe.serhal@netlify.com>
2024-05-22 14:52:13 -0400

    add user Netlify Function and Edge Functions

---
+++ netlify/edge-functions/response-doubler.ts
@@ -0,0 +1,11 @@
+import type { Config, Context } from "@netlify/edge-functions";
+
+export default async (_req: Request, context: Context) => {
+  const response = await context.next();
+  const text = await response.text();
+  return new Response(`${text}${text}`, response);
+};
+
+export const config: Config = {
+  path: ["/hello-user", "/boop-user"],
+};
